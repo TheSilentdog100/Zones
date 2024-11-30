90a172209c1bef098b0debb9b788ee0b90f92a23
Martin Steineck <steineck.martin@gmail.com>
2024-11-30 22:12:54 +0100

    Update core.lua
    Fixed the Zones list

--- core.lua
+++ core.lua
@@ -1,95 +1,92 @@
 local currentXP = 0
-local playerLevel =  UnitLevel("player")
+local playerLevel = UnitLevel("player")
 
 local faction = UnitFactionGroup("player")
 
 local isEng = true
 
-local csvDataGerman =  {
-    { "Dun Morogh", "Öst. Königreiche", 1, 10, 5.5, "A" },
-    { "Durotar", "Kalimdor", 1, 10, 5.5, "H" },
-    { "Mulgore", "Kalimdor", 1, 10, 5.5, "H" },
-    { "Teldrassil", "Kalimdor", 1, 10, 5.5, "A" },
-    { "Tirisfal", "Öst. Königreiche", 1, 10, 5.5, "H" },
-    { "Wald von Elwynn", "Öst. Königreiche", 1, 10, 5.5, "A" },
-    { "Dunkelküste", "Kalimdor", 10, 20, 15, "A" },
-    { "Loch Modan", "Öst. Königreiche", 10, 20, 15, "A" },
-    { "Silberwald", "Öst. Königreiche", 10, 20, 15, "A" },
-    { "Westfall", "Öst. Königreiche", 10, 20, 15, "A" },
-    { "Brachland", "Kalimdor", 10, 25, 17.5, "A/H" },
-    { "Rotkammgebirge", "Öst. Königreiche", 15, 25, 20, "A" },
-    { "Steinkrallengebirge", "Kalimdor", 15, 27, 21, "H" },
-    { "Dämmerwald", "Öst. Königreiche", 18, 30, 24, "A" },
-    { "Eschental", "Kalimdor", 18, 30, 24, "A" },
-    { "Sumpfland", "Öst. Königreiche", 20, 30, 25, "A" },
-    { "Vorgebirge des Hügellands", "Öst. Königreiche", 20, 30, 25, "A" },
-    { "Tausend Nadeln", "Kalimdor", 25, 35, 30, "A" },
-    { "Alteracgebirge", "Öst. Königreiche", 30, 40, 35, "A" },
-    { "Arathihochland", "Öst. Königreiche", 30, 40, 35, "A" },
-    { "Desolace", "Kalimdor", 30, 40, 35, "H" },
-    { "Schlingendorntal", "Öst. Königreiche", 30, 45, 37.5, "A" },
-    { "Marschen von Dustwallow", "Kalimdor", 35, 45, 40, "A" },
-    { "Sümpfe des Elends", "Öst. Königreiche", 35, 45, 40, "A" },
-    { "Ödland", "Öst. Königreiche", 35, 45, 40, "A" },
-    { "Hinterland", "Öst. Königreiche", 40, 50, 45, "A" },
-    { "Tanaris", "Kalimdor", 40, 50, 45, "A" },
-    { "Feralas", "Kalimdor", 42, 50, 46, "A" },
-    { "Sengende Schlucht", "Öst. Königreiche", 45, 50, 47.5, "A" },
-    { "Azshara", "Kalimdor", 45, 55, 50, "A" },
-    { "Verwüstete Lande", "Öst. Königreiche", 45, 55, 50, "A" },
-    { "Teufelswald", "Kalimdor", 48, 55, 51.5, "A" },
-    { "Un'Goro-Krater", "Kalimdor", 48, 55, 51.5, "A" },
-    { "Brennende Steppe", "Öst. Königreiche", 50, 58, 54, "A" },
-    { "Westl. Pestländer", "Öst. Königreiche", 51, 58, 54.5, "A" },
-    { "Östl. Pestländer", "Öst. Königreiche", 53, 60, 56.5, "A" },
-    { "Winterspring", "Kalimdor", 53, 60, 56.5, "A" },
-    { "Gebirgspass der Totenwinde", "Öst. Königreiche", 55, 60, 57.5, "A" },
-    { "Moonglade", "Kalimdor", 55, 60, 57.5, "A" },
-    { "Silithus", "Kalimdor", 55, 60, 57.5, "A" }
+local csvDataGerman = {
+    { "Dun Morogh",           "Östliche Königreiche", 1,  10, 5.5,  "A" },
+    { "Durotar",              "Kalimdor",             1,  10, 5.5,  "H" },
+    { "Mulgore",              "Kalimdor",             1,  10, 5.5,  "H" },
+    { "Teldrassil",           "Kalimdor",             1,  10, 5.5,  "A" },
+    { "Tirisfal",             "Östliche Königreiche", 1,  10, 5.5,  "H" },
+    { "Wald von Elwynn",      "Östliche Königreiche", 1,  10, 5.5,  "A" },
+    { "Dunkelküste",          "Kalimdor",             10, 20, 15,   "A" },
+    { "Loch Modan",           "Östliche Königreiche", 10, 20, 15,   "A" },
+    { "Silberwald",           "Östliche Königreiche", 10, 20, 15,   "H" },
+    { "Westfall",             "Östliche Königreiche", 10, 20, 15,   "A" },
+    { "Brachland",            "Kalimdor",             10, 25, 17.5, "A/H" },
+    { "Rotkammgebirge",       "Östliche Königreiche", 15, 25, 20,   "A" },
+    { "Steinkrallengebirge",  "Kalimdor",             15, 27, 21,   "H" },
+    { "Dämmerwald",           "Östliche Königreiche", 18, 30, 24,   "A" },
+    { "Eschental",            "Kalimdor",             18, 30, 24,   "A/H" },
+    { "Sumpfland",            "Östliche Königreiche", 20, 30, 25,   "A" },
+    { "Vorgebirge des Hügellands", "Östliche Königreiche", 20, 30, 25, "H" },
+    { "Tausend Nadeln",       "Kalimdor",             25, 35, 30,   "A/H" },
+    { "Arathihochland",       "Östliche Königreiche", 30, 40, 35,   "A/H" },
+    { "Desolace",             "Kalimdor",             30, 40, 35,   "A/H" },
+    { "Schlingendorntal",     "Östliche Königreiche", 30, 45, 37.5, "A/H" },
+    { "Düstermarschen",       "Kalimdor",             35, 45, 40,   "A/H" },
+    { "Sümpfe des Elends",    "Östliche Königreiche", 35, 45, 40,   "A/H" },
+    { "Ödland",               "Östliche Königreiche", 35, 45, 40,   "A/H" },
+    { "Hinterland",           "Östliche Königreiche", 40, 50, 45,   "A/H" },
+    { "Tanaris",              "Kalimdor",             40, 50, 45,   "A/H" },
+    { "Feralas",              "Kalimdor",             42, 50, 46,   "A/H" },
+    { "Brennende Steppe",     "Östliche Königreiche", 45, 50, 47.5, "A/H" },
+    { "Azshara",              "Kalimdor",             45, 55, 50,   "H" },
+    { "Verwüstete Lande",     "Östliche Königreiche", 45, 55, 50,   "A/H" },
+    { "Teufelswald",          "Kalimdor",             48, 55, 51.5, "A/H" },
+    { "Krater von Un'Goro",   "Kalimdor",             48, 55, 51.5, "A/H" },
+    { "Westliche Pestländer", "Östliche Königreiche", 50, 58, 54,   "A/H" },
+    { "Östliche Pestländer",  "Östliche Königreiche", 51, 58, 54.5, "A/H" },
+    { "Winterspring",         "Kalimdor",             53, 60, 56.5, "A/H" },
+    { "Verwüstete Lande",     "Östliche Königreiche", 53, 60, 56.5, "A/H" },
+    { "Mondlichtung",         "Kalimdor",             55, 60, 57.5, "A/H" },
+    { "Silithus",             "Kalimdor",             55, 60, 57.5, "A/H" }
 }
+
 
 local csvDataEnglish = {
-    { "Dun Morogh", "Eastern Kingdoms", 1, 10, 5.5, "A" },
-    { "Durotar", "Kalimdor", 1, 10, 5.5, "H" },
-    { "Mulgore", "Kalimdor", 1, 10, 5.5, "H" },
-    { "Teldrassil", "Kalimdor", 1, 10, 5.5, "A" },
-    { "Tirisfal Glades", "Eastern Kingdoms", 1, 10, 5.5, "H" },
-    { "Elwynn Forest", "Eastern Kingdoms", 1, 10, 5.5, "A" },
-    { "Darkshore", "Kalimdor", 10, 20, 15, "A" },
-    { "Loch Modan", "Eastern Kingdoms", 10, 20, 15, "A" },
-    { "Silverpine Forest", "Eastern Kingdoms", 10, 20, 15, "H" },
-    { "Westfall", "Eastern Kingdoms", 10, 20, 15, "A" },
-    { "The Barrens", "Kalimdor", 10, 25, 17.5, "A/H" },
-    { "Redridge Mountains", "Eastern Kingdoms", 15, 25, 20, "A" },
-    { "Stonetalon Mountains", "Kalimdor", 15, 27, 21, "H" },
-    { "Duskwood", "Eastern Kingdoms", 18, 30, 24, "A" },
-    { "Ashenvale", "Kalimdor", 18, 30, 24, "A" },
-    { "Wetlands", "Eastern Kingdoms", 20, 30, 25, "A" },
-    { "Hillsbrad Foothills", "Eastern Kingdoms", 20, 30, 25, "A" },
-    { "Thousand Needles", "Kalimdor", 25, 35, 30, "A" },
-    { "Arathi Highlands", "Eastern Kingdoms", 30, 40, 35, "A" },
-    { "Desolace", "Kalimdor", 30, 40, 35, "H" },
-    { "Stranglethorn Vale", "Eastern Kingdoms", 30, 45, 37.5, "A" },
-    { "Dustwallow Marsh", "Kalimdor", 35, 45, 40, "A" },
-    { "Swamp of Sorrows", "Eastern Kingdoms", 35, 45, 40, "A" },
-    { "Badlands", "Eastern Kingdoms", 35, 45, 40, "A" },
-    { "The Hinterlands", "Eastern Kingdoms", 40, 50, 45, "A" },
-    { "Tanaris", "Kalimdor", 40, 50, 45, "A" },
-    { "Feralas", "Kalimdor", 42, 50, 46, "A" },
-    { "Burning Steppes", "Eastern Kingdoms", 45, 50, 47.5, "A" },
-    { "Azshara", "Kalimdor", 45, 55, 50, "A" },
-    { "Blasted Lands", "Eastern Kingdoms", 45, 55, 50, "A" },
-    { "Felwood", "Kalimdor", 48, 55, 51.5, "A" },
-    { "Un'Goro Crater", "Kalimdor", 48, 55, 51.5, "A" },
-    { "Western Plaguelands", "Eastern Kingdoms", 50, 58, 54, "A" },
-    { "Eastern Plaguelands", "Eastern Kingdoms", 51, 58, 54.5, "A" },
-    { "Winterspring", "Kalimdor", 53, 60, 56.5, "A" },
-    { "Blasted Lands", "Eastern Kingdoms", 53, 60, 56.5, "A" },
-    { "Western Plaguelands", "Eastern Kingdoms", 55, 60, 57.5, "A" },
-    { "Moonglade", "Kalimdor", 55, 60, 57.5, "A" },
-    { "Silithus", "Kalimdor", 55, 60, 57.5, "A" }
+    { "Dun Morogh",           "Eastern Kingdoms", 1,  10, 5.5,  "A" },
+    { "Durotar",              "Kalimdor",         1,  10, 5.5,  "H" },
+    { "Mulgore",              "Kalimdor",         1,  10, 5.5,  "H" },
+    { "Teldrassil",           "Kalimdor",         1,  10, 5.5,  "A" },
+    { "Tirisfal Glades",      "Eastern Kingdoms", 1,  10, 5.5,  "H" },
+    { "Elwynn Forest",        "Eastern Kingdoms", 1,  10, 5.5,  "A" },
+    { "Darkshore",            "Kalimdor",         10, 20, 15,   "A" },
+    { "Loch Modan",           "Eastern Kingdoms", 10, 20, 15,   "A" },
+    { "Silverpine Forest",    "Eastern Kingdoms", 10, 20, 15,   "H" },
+    { "Westfall",             "Eastern Kingdoms", 10, 20, 15,   "A" },
+    { "The Barrens",          "Kalimdor",         10, 25, 17.5, "A/H" },
+    { "Redridge Mountains",   "Eastern Kingdoms", 15, 25, 20,   "A" },
+    { "Stonetalon Mountains", "Kalimdor",         15, 27, 21,   "H" },
+    { "Duskwood",             "Eastern Kingdoms", 18, 30, 24,   "A" },
+    { "Ashenvale",            "Kalimdor",         18, 30, 24,   "A/H" },
+    { "Wetlands",             "Eastern Kingdoms", 20, 30, 25,   "A" },
+    { "Hillsbrad Foothills",  "Eastern Kingdoms", 20, 30, 25,   "H" },
+    { "Thousand Needles",     "Kalimdor",         25, 35, 30,   "A/H" },
+    { "Arathi Highlands",     "Eastern Kingdoms", 30, 40, 35,   "A/H" },
+    { "Desolace",             "Kalimdor",         30, 40, 35,   "A/H" },
+    { "Stranglethorn Vale",   "Eastern Kingdoms", 30, 45, 37.5, "A/H" },
+    { "Dustwallow Marsh",     "Kalimdor",         35, 45, 40,   "A/H" },
+    { "Swamp of Sorrows",     "Eastern Kingdoms", 35, 45, 40,   "A/H" },
+    { "Badlands",             "Eastern Kingdoms", 35, 45, 40,   "A/H" },
+    { "The Hinterlands",      "Eastern Kingdoms", 40, 50, 45,   "A/H" },
+    { "Tanaris",              "Kalimdor",         40, 50, 45,   "A/H" },
+    { "Feralas",              "Kalimdor",         42, 50, 46,   "A/H" },
+    { "Burning Steppes",      "Eastern Kingdoms", 45, 50, 47.5, "A/H" },
+    { "Azshara",              "Kalimdor",         45, 55, 50,   "H" },
+    { "Blasted Lands",        "Eastern Kingdoms", 45, 55, 50,   "A/H" },
+    { "Felwood",              "Kalimdor",         48, 55, 51.5, "A/H" },
+    { "Un'Goro Crater",       "Kalimdor",         48, 55, 51.5, "A/H" },
+    { "Western Plaguelands",  "Eastern Kingdoms", 50, 58, 54,   "A/H" },
+    { "Eastern Plaguelands",  "Eastern Kingdoms", 51, 58, 54.5, "A/H" },
+    { "Winterspring",         "Kalimdor",         53, 60, 56.5, "A/H" },
+    { "Blasted Lands",        "Eastern Kingdoms", 53, 60, 56.5, "A/H" },
+    { "Moonglade",            "Kalimdor",         55, 60, 57.5, "A/H" },
+    { "Silithus",             "Kalimdor",         55, 60, 57.5, "A/H" }
 }
-
 
 local function suitableZones()
     local completeLevelZone = ""
@@ -104,14 +101,19 @@ local function suitableZones()
         ContinentString = ", Continent: "
     else
         csv = csvDataGerman
-        zoneString= "Gebiet: "
+        zoneString = "Gebiet: "
         ContinentString = ", Kontinent: "
     end
 
     for _, line in ipairs(csv) do
-        if (faction == "Alliance" and line[6] == "A") or (faction == "Horde" and line[6] == "H") then
+        if (faction == "Alliance" and (line[6] == "A" or line[6] == "A/H")) or
+            (faction == "Horde" and (line[6] == "H" or line[6] == "A/H")) then
             if playerLevel >= line[3] and playerLevel <= line[4] then
-                completeLevelZone = "\n" .. zoneString .. tostring(line[1]) .. ContinentString .. tostring(line[2]) .. ", Min: " .. tostring(line[3]) .. ", Max: " .. tostring(line[4]) .. "\n"
+                completeLevelZone = "\n" ..
+                zoneString ..
+                tostring(line[1]) ..
+                ContinentString ..
+                tostring(line[2]) .. ", Min: " .. tostring(line[3]) .. ", Max: " .. tostring(line[4]) .. "\n"
                 print(completeLevelZone)
             end
         end
@@ -138,4 +140,5 @@ end
 SLASH_ZONE1 = "/zones"
 SlashCmdList["ZONE"] = commandReacter
 
-print("Zones is running! Type /zones to see all suitable leveling zones. \nToggle between German and English using /zones ger and /zones eng.")
+print(
+"Zones is running! Type /zones to see all suitable leveling zones. \nToggle between German and English using /zones ger and /zones eng.")
